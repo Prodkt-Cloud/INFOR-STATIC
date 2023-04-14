17ab80ec82de735e2273ae7ce279e04a9ed147ad
Prodkt Team <101760605+prodkt@users.noreply.github.com>
2023-04-14 16:50:06 -0600

    updates before jumping branches

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import UnoCSS from "@unocss/astro";
+import alpinejs from "@astrojs/alpinejs";
 import image from "@astrojs/image";
 import pagefind from "astro-pagefind";
 import vercel from "@astrojs/vercel/static";
-
-import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
