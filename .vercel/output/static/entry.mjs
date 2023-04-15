bee36766872e44301f4bbc79f0c560a6a2e018c7
Prodkt Team <technology@prodkt.cloud>
2023-04-15 09:01:45 -0600

    BUILD TIME!

--- .vercel/output/static/entry.mjs
+++
@@ -1,9 +0,0 @@
-import { s as server_default } from './chunks/astro.e81a75ab.mjs';
-import { _ as _page0, a as _page1, b as _page2, c as _page3, d as _page4, e as _page5, f as _page6, g as _page7, h as _page8, i as _page9, j as _page10, k as _page11, l as _page12, m as _page13, n as _page14, o as _page15, p as _page16 } from './chunks/pages/all.81cd2042.mjs';
-import 'html-escaper';
-/* empty css                                 */import 'node:path';
-/* empty css                                 */
-const pageMap = new Map([["src/pages/index.astro", _page0],["src/pages/learning-paths/index.astro", _page1],["src/pages/learning-paths/courses/index.astro", _page2],["src/pages/learning-paths/courses/[...slug].astro", _page3],["src/pages/learning-paths/[...slug].astro", _page4],["src/pages/learning-paths/[...industry].astro", _page5],["src/pages/industries/index.astro", _page6],["src/pages/industries/[...tag].astro", _page7],["src/pages/industries/[...slug].astro", _page8],["src/pages/searchtest.astro", _page9],["src/pages/lifecycle/index.astro", _page10],["src/pages/lifecycle/[...tag].astro", _page11],["src/pages/lifecycle/[...slug].astro", _page12],["src/pages/roles/index.astro", _page13],["src/pages/roles/[...tag].astro", _page14],["src/pages/roles/[...slug].astro", _page15],["src/pages/roles/[...industry].astro", _page16],]);
-const renderers = [Object.assign({"name":"astro:jsx","serverEntrypoint":"astro/jsx/server.js","jsxImportSource":"astro"}, { ssr: server_default }),];
-
-export { pageMap, renderers };
